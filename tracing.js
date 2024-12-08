fe129f9e947b5a1682c4cb44ba00e971f4d2370c
CohenAdkins <Cohen.adkins@ufl.edu>
2024-12-07 19:21:13 -0500

    assignment 12 code

---
+++ tracing.js
@@ -0,0 +1,58 @@
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { trace } = require("@opentelemetry/api");
+//Instrumentations
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
+const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+//Exporter
+module.exports = (serviceName) => {
+    const exporter = new JaegerExporter({
+        serviceName: serviceName,
+        endpoint: "http://localhost:14268/api/traces"
+    });
+
+    const provider = new NodeTracerProvider({
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+        }),
+    });
+    provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+    provider.register();
+
+    registerInstrumentations({
+       instrumentations: [
+           new HttpInstrumentation(),
+           new ExpressInstrumentation(),
+           new MongoDBInstrumentation(),
+       ],
+       tracerProvider: provider,
+    });
+   
+    return trace.getTracer(serviceName);
+};
+
+// import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+
+// const options = {
+//   tags: [], // optional
+//   // You can use the default UDPSender
+//   host: 'localhost', // optional
+//   port: 6832, // optional
+//   // OR you can use the HTTPSender as follows
+//   // endpoint: 'http://localhost:14268/api/traces',
+//   maxPacketSize: 65000 // optional
+// }
+// const exporter = new JaegerExporter(options);
+// Now, register the exporter.
+
+// const tracerProvider = new NodeTracerProvider({
+//   spanProcessors: [new BatchSpanProcessor(exporter)]
+// })
+
+// const tracer = traceProvider.getTracer('my-tracer')
